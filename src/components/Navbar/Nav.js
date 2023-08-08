84293b53ec01df71ac1fca65ef7390fa064b4273
Neha <nehatualea@gmail.com>
2023-08-09 01:17:29 +0530

    formatted document

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import style from './navbar.module.css'
+import React from "react";
+import style from "./navbar.module.css";
 
 const Nav = () => {
   return (
-   <>
-   <nav>
-    <div className={style.container}>
-        <div >
-            <img src="https://cdn.dribbble.com/users/1985786/screenshots/5363979/gif.gif" alt="bliss-bond" width={50} height={50} className={style.logo}/>
+    <>
+      <nav>
+        <div className={style.container}>
+          <div>
+            <img
+              src="https://cdn.dribbble.com/users/1985786/screenshots/5363979/gif.gif"
+              alt="bliss-bond"
+              width={50}
+              height={50}
+              className={style.logo}
+            />
             <figcaption>Bliss Bond</figcaption>
+          </div>
 
+          <div>serch</div>
+
+          <div>account</div>
         </div>
+      </nav>
+    </>
+  );
+};
 
-        <div>
-            serch
-        </div>
-
-        <div>
-            account
-        </div>
-
-    </div>
-   </nav>
-   </>
-  )
-}
-
-export default Nav
+export default Nav;
