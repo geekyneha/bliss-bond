4171dde5f018ef73fd40de95d35a798910fbb46e
Neha <nehatualea@gmail.com>
2023-08-09 01:17:55 +0530

    created : search bar

---
+++ src/components/serch/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./search.module.css";
+
+const Search = () => {
+  return (
+    <divst className={style.search}>
+      <input type="text" name="" id="" placeholder="Search destinations" />
+      <button>Check in date</button>
+      <button>Check out date</button>
+      <button>Add guests</button>
+    </divst>
+  );
+};
+
+export default Search;
