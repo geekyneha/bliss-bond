62cbf5efb3bade28e60a48c7b1636ea254d640f2
Neha <nehatualea@gmail.com>
2023-08-10 00:06:45 +0530

    styled: Search

--- src/components/serch/Search.js
+++ src/components/serch/Search.js
@@ -3,12 +3,20 @@ import style from "./search.module.css";
 
 const Search = () => {
   return (
-    <divst className={style.search}>
-      <input type="text" name="" id="" placeholder="Search destinations" />
-      <button>Check in date</button>
-      <button>Check out date</button>
-      <button>Add guests</button>
-    </divst>
+    <div className={style.search}>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Search destinations"
+        className={style["rounded"]}
+      />
+      <button className={style["rounded"]}>Check in date</button>
+      <button className={style["rounded"]}>Check out date</button>
+      <button className={style["guest"]}>
+        Add guests <button className={style["search-button"]}>Serch</button>
+      </button>
+    </div>
   );
 };
 
