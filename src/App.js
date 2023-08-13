fe688a859fbf6d296a104958947c6f6719fe77ae
Neha <nehatualea@gmail.com>
2023-08-14 01:56:02 +0530

    used : outlet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import Nav from "../src/components/Navbar/Nav";
+import Services from "../src/components/services/Services";
 
 function App() {
   return (
     <>
-      <Home />
+      <Nav />
+      <Services />
+      <Outlet />
     </>
   );
 }
