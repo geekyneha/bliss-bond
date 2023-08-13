bd945e973498f4b8ea8501c2f34c75cac6a5cad2
Neha <nehatualea@gmail.com>
2023-08-14 01:57:11 +0530

    updated : constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -15,15 +15,40 @@ import {
 import { FaHotel } from "react-icons/fa";
 import { BsFillCameraReelsFill } from "react-icons/bs";
 export const services = [
-  { id: uuidv4(), icon: GiLovers, label: "Wedding" },
-  { id: uuidv4(), icon: GiBigDiamondRing, label: "Engagement" },
-  { id: uuidv4(), icon: GiWinterGloves, label: "Haldi" },
-  { id: uuidv4(), icon: GiLoveLetter, label: "Invitations" },
-  { id: uuidv4(), icon: MdFastfood, label: "Food" },
+  {
+    id: uuidv4(),
+    icon: GiLovers,
+    label: "Wedding",
+    path: "/weddingDecoration",
+  },
+  {
+    id: uuidv4(),
+    icon: GiBigDiamondRing,
+    label: "Engagement",
+    path: "/engagementDecoration",
+  },
+  {
+    id: uuidv4(),
+    icon: GiWinterGloves,
+    label: "Haldi",
+    path: "/haldiDecoration",
+  },
+  {
+    id: uuidv4(),
+    icon: GiLoveLetter,
+    label: "Invitations",
+    path: "/invitation",
+  },
+  { id: uuidv4(), icon: MdFastfood, label: "Food", path: "/food" },
   { id: uuidv4(), icon: FaHotel, label: "Stay" },
   { id: uuidv4(), icon: MdEmojiTransportation, label: "Transportation" },
   { id: uuidv4(), icon: MdPlace, label: "Destination" },
-  { id: uuidv4(), icon: BsFillCameraReelsFill, label: "Casset" },
-  { id: uuidv4(), icon: MdFace2, label: "Beautician" },
-  { id: uuidv4(), icon: GiVineFlower, label: "Henna" },
+  {
+    id: uuidv4(),
+    icon: BsFillCameraReelsFill,
+    label: "filmer",
+    path: "/videoGrapher",
+  },
+  { id: uuidv4(), icon: MdFace2, label: "Beautician", path: "/makeupArtist" },
+  { id: uuidv4(), icon: GiVineFlower, label: "Henna", path: "/mehendiArtist" },
 ];
