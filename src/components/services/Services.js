479c46f8659dd8f191b9e1791ca9b6af6067352c
Neha <nehatualea@gmail.com>
2023-08-14 01:55:23 +0530

    updated : serch and services

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,16 +1,19 @@
 import React from "react";
 import style from "./services.module.css";
 import { services } from "../../constants/constants";
+import { Link } from "react-router-dom";
 
 const Services = () => {
   return (
     <>
       <div className={style["service-container"]}>
         {services.map((service, id) => (
-          <div key={service.id} className={style["service"]}>
-            <service.icon size={25} fill="gray" />
-            <p>{service.label}</p>
-          </div>
+          <Link to={service.path} className={style["link"]}>
+            <div key={service.id} className={style["service"]}>
+              <service.icon size={25} fill="gray" />
+              <div>{service.label}</div>
+            </div>
+          </Link>
         ))}
       </div>
     </>
