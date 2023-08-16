270bf2ca39029b0029cdc87319685f411d6135b0
Neha <nehatualea@gmail.com>
2023-08-16 22:22:25 +0530

    styled: nav bar

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -3,33 +3,39 @@ import style from "./navbar.module.css";
 import Search from "../serch/Search";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { AiTwotoneSnippets } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <>
       <nav>
         <div className={style.container}>
-          <div>
-            <img
-              src="https://cdn.dribbble.com/users/1985786/screenshots/5363979/gif.gif"
-              alt="bliss-bond"
-              width={50}
-              height={50}
-              className={style.logo}
-            />
-            <figcaption className={style["brand-name"]}>Bliss Bond</figcaption>
-          </div>
+          <Link to={"/"} className={style["link"]}>
+            <div>
+              <img
+                src="https://cdn.dribbble.com/users/1985786/screenshots/5363979/gif.gif"
+                alt="bliss-bond"
+                width={50}
+                height={50}
+                className={style.logo}
+              />
+              <figcaption className={style["brand-name"]}>
+                Bliss Bond
+              </figcaption>
+            </div>
+          </Link>
 
           <div>
             <Search />
           </div>
+          <div className={style["right-options"]}>
+            <div className={style["booking"]}>
+              <AiTwotoneSnippets size={25} fill="#FE6180" />
+            </div>
 
-          <div className={style["booking"]}>
-            <AiTwotoneSnippets size={25} fill="#FE6180" />
-          </div>
-
-          <div className={style["account"]}>
-            <BsFillPersonLinesFill size={25} />
+            <div className={style["account"]}>
+              <BsFillPersonLinesFill size={25} />
+            </div>
           </div>
         </div>
       </nav>
