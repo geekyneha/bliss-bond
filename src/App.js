f433708face42fb472d5556308ca635652a841c4
Neha <nehatualea@gmail.com>
2023-08-21 20:59:06 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import Nav from "../src/components/Navbar/Nav";
 import Services from "../src/components/services/Services";
 import Footer from "../src/components/Footer/Footer";
 
-
 function App() {
   return (
-    
     <>
-   
       <Nav />
       <Services />
       <Outlet />
