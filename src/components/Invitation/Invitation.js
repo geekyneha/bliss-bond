3b061eb61fc34f7790b633649809a938fd7f2ac6
Neha <nehatualea@gmail.com>
2023-08-23 15:48:16 +0530

    adedd: pagination

--- src/components/Invitation/Invitation.js
+++ src/components/Invitation/Invitation.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetchData from "../../hooks/useFetchData";
 import style from "./invitation.module.css";
+import Pagination from "../Pagination/Pagination";
 
 export const Invitation = ({ endpoint }) => {
   const info = useFetchData(endpoint);
-  console.log(info);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(16);
+
+  console.log(info.length);
+
+  const lastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = lastPostIndex - postPerPage;
+
+  const currentPosts = info.slice(firstPostIndex, lastPostIndex);
   return (
-    <div className={style.container}>
-      {info.map((data, index) => (
-        <div key={index}>
-          <img
-            src={data.imageUrl}
-            alt="invtation"
-            width={150}
-            className={style.image}
-          />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className={style.container}>
+        {currentPosts.map((data, index) => (
+          <div key={index}>
+            <img
+              src={data.imageUrl}
+              alt="invtation"
+              width={150}
+              className={style.image}
+            />
+          </div>
+        ))}
+      </div>
+
+      <Pagination
+        totalPosts={info.length}
+        postPerPage={postPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
+    </>
   );
 };
