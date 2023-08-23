e62fbedd5bf97b52aced9b5bd1190662c856d380
Neha <nehatualea@gmail.com>
2023-08-23 15:47:48 +0530

    added: scroll on click

--- src/Section/Testimonial/Testimonials.js
+++ src/Section/Testimonial/Testimonials.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import style from "./testimonials.module.css";
 import useFetchData from "../../hooks/useFetchData";
+import { BiSolidLeftArrow, BiSolidRightArrow } from "react-icons/bi";
 
 const Testimonials = ({ endpoint }) => {
   const info = useFetchData(endpoint);
+  const testimonialRef = useRef(null);
+
+  const scrollLeft = () => {
+    testimonialRef.current.scrollBy({
+      left: 500,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollRight = () => {
+    testimonialRef.current.scrollBy({
+      left: -500,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <section className={style["section"]}>
-      <h1>Hear from Our custmers</h1>
-      <div className={style["testimonial"]}>
+      <BiSolidLeftArrow
+        size={25}
+        className={style["scroll-left"]}
+        onClick={scrollRight}
+      />
+      <div className={style["testimonial"]} ref={testimonialRef}>
         {info.map((feedback) => (
           <div className={style["card"]} key={feedback.id}>
             <div className={style["feedBack"]}>
@@ -28,6 +49,11 @@ const Testimonials = ({ endpoint }) => {
           </div>
         ))}
       </div>
+      <BiSolidRightArrow
+        size={25}
+        className={style["scroll-right"]}
+        onClick={scrollLeft}
+      />
     </section>
   );
 };
