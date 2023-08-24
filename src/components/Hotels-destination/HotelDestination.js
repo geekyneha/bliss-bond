c54b94905254898b5bf46ae7469936d5fcd42a8f
Neha <nehatualea@gmail.com>
2023-08-24 21:42:26 +0530

    added: shimmer UI

--- src/components/Hotels-destination/HotelDestination.js
+++ src/components/Hotels-destination/HotelDestination.js
@@ -2,36 +2,33 @@ import React from "react";
 import useFetchData from "../../hooks/useFetchData";
 import style from "./hotel-destination.module.css";
 import { BsStarFill } from "react-icons/bs";
+import DecorationShimmer from "../Decoration/DecorationShimmer";
 
 const HotelDestination = ({ endpoint }) => {
   const info = useFetchData(endpoint);
-  // console.log(info[0].hotels[0].imageUrl);
-
-  // const hotelInfo = info[0].hotels;
-  // console.log(hotelInfo)
 
   return (
     <div className={style["container"]}>
-      {info[0]?.hotels?.map((data, index) => (
-        <div className={style["card"]} key={index}>
-          <img
-            alt="hotel-image"
-            src={data.imageUrl}
-            className={style["decoration-image"]}
-          />
+      {info.length === 0
+        ? Array(20)
+            .fill()
+            .map((_, index) => <DecorationShimmer key={index} />)
+        : info[0]?.hotels?.map((data, index) => (
+            <div className={style["card"]} key={index}>
+              <img alt="hotel" src={data.imageUrl} className={style["hotel"]} />
 
-          <div className={style["decoration-detail"]}>
-            <span>{data.name}</span>
-            <div className={style["rating"]}>
-              <div className={style.star}>
-                <BsStarFill fill="#fff" />
+              <div className={style["decoration-detail"]}>
+                <span>{data.name}</span>
+                <div className={style["rating"]}>
+                  <div className={style.star}>
+                    <BsStarFill fill="#fff" />
+                  </div>
+                  <div className={style["rate"]}>{data.rating}</div>
+                </div>
               </div>
-              <div className={style["rate"]}>{data.rating}</div>
+              <div className={style["cost"]}>₹{data.cost_per_night}</div>
             </div>
-          </div>
-          <div className={style["cost"]}>₹{data.cost_per_night}</div>
-        </div>
-      ))}
+          ))}
     </div>
   );
 };
--- src/components/Transportation/Transportation.js
+++ src/components/Transportation/Transportation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./transportation.module.css";
 import useFetchData from "../../hooks/useFetchData";
-import { BsStarFill } from "react-icons/bs";
+import DecorationShimmer from "../Decoration/DecorationShimmer";
 
 const Transportation = ({ endpoint }) => {
   const info = useFetchData(endpoint);
@@ -9,26 +9,30 @@ const Transportation = ({ endpoint }) => {
 
   return (
     <div className={style["container"]}>
-      {info.map((data) => (
-        <div className={style["card"]} key={data.id}>
-          <img
-            alt="decoration"
-            src={data.image_url}
-            className={style["transportaion-image"]}
-          />
+      {info.length === 0
+        ? Array(20)
+            .fill()
+            .map((_, index) => <DecorationShimmer key={index} />)
+        : info.map((data) => (
+            <div className={style["card"]} key={data.id}>
+              <img
+                alt="decoration"
+                src={data.image_url}
+                className={style["transportaion-image"]}
+              />
 
-          <div className={style["transportaion-detail"]}>
-            <div className={style["detail"]}>
-              <div className={style.star}>{data.company_name}</div>
-              <div className={style["pricing"]}>From {data.pricing}</div>
+              <div className={style["transportaion-detail"]}>
+                <div className={style["detail"]}>
+                  <div className={style.star}>{data.company_name}</div>
+                  <div className={style["pricing"]}>From {data.pricing}</div>
+                </div>
+                <div></div>
+                <div>{data.vehicle_type}</div>
+
+                {/* <button className={style["button"]}>Request Pricing</button> */}
+              </div>
             </div>
-            <div></div>
-            <div>{data.vehicle_type}</div>
-
-            <button className={style["button"]}>Request Pricing</button>
-          </div>
-        </div>
-      ))}
+          ))}
     </div>
   );
 };
